0d497bb2b3e1f5867a117ae023ee50e92eb84523
dakotash <dakotash@email.sc.edu>
2023-01-15 23:18:18 -0500

    added some more java doc

--- strategy/Character.java
+++ strategy/Character.java
@@ -1,21 +1,45 @@
 package strategy;
+/**
+ * The parent class for medievel charcters in a game
+ * @author Cody Hawkins
+ * 
+ */
 public class Character implements WeaponBehavior{
 	private String name;
 	private WeaponBehavior weaponBehavior;
 	
+	/**
+	 * Sets the charcters name
+	 * @param name
+	 * 
+	 */
 	public Character(String name) {
 	this.name = name;
 	}
 	
+	/**
+	 * Sets the charcters attack method
+	 * @returns string
+	 * 
+	 */
 	public String attack() {
 		return "";
 	}
 	
+	/**
+	 * sets characters weapon behavior
+	 * @param wb
+	 */
 	public void setWeaponBehavior(WeaponBehavior wb) {
 		this.weaponBehavior = wb;
 		
 	}
 	
+	/**
+	 * Creates a string to represent the character 
+	 * @returns a string of the name
+	 * 
+	 */
 	public String toString() {
 		return name;
 	}
